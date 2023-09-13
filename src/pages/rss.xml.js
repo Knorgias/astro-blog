095e2f45c065b45723bd803906301968ea07f213
Knorgias <konstantinos.norgias@ing.com>
2023-09-13 14:03:48 +0200

    update RSS feed to point to /blog/

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'Knorgias journey learning Astro 🧑‍🚀✨',
-    site: 'https://knorgias-astro-blog.netlify.app/',
+    site: 'https://knorgias-astro-blog.netlify.app/blog/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
