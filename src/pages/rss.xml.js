92e061029280fada16cdcbfd9687f908cf9a45cb
Knorgias <konstantinos.norgias@ing.com>
2023-09-13 13:45:34 +0200

    create RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'Knorgias journey learning Astro 🧑‍🚀✨',
+    site: 'https://knorgias-astro-blog.netlify.app/',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
